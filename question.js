d30a1643cedc9b0d86287afba9657b37cd1a2b40
Shruti Soni <130765736+shrutisoni08@users.noreply.github.com>
2023-10-12 22:04:43 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,542 @@
+const Pipes_And_cisterns= [
+    {
+        'que': 'Two pipes A and B can fill a cistern in 37 minutes and 45 minutes respectively. Both pipes are opened. The cistern will be filled in just half an hour, if the B is turned off after:',
+        'a': '5 Min.',
+        'b': '9 Min',
+        'c': '6 Min',
+        'd': '8 Min.',
+        'correct': 'b'
+    },
+    {
+        'que': 'Two pipes can fill a tank in 20 and 24 minutes respectively and a waste pipe can empty 3 gallons per minute. All the three pipes working together can fill the tank in 15 minutes. The capacity of the tank is:',
+        'a': '60 Gallons',
+        'b': '100 Gallons',
+        'c': '120 Gallons',
+        'd': '180 Gallons',
+        'correct': 'c'
+    },
+    {
+        'que': 'A tank is filled in 5 hours by three pipes A, B and C. The pipe C is twice as fast as B and B is twice as fast as A. How much time will pipe A alone take to fill the tank?',
+        'a': '20 hours',
+        'b': '25 hours',
+        'c': '35 hours',
+        'd': 'Cannot Be Determind',
+        'correct': 'c'
+    },
+    {
+        'que': 'Two pipes A and B together can fill a cistern in 4 hours. Had they been opened separately, then B would have taken 6 hours more than A to fill the cistern. How much time will be taken by A to fill the cistern separately?',
+        'a': '1 hours',
+        'b': '2 hours',
+        'c': '4 hours',
+        'd': '6 hours',
+        'correct': 'd'
+    },
+    {
+        'que': 'Two pipes A and B can fill a tank in 20 and 30 minutes respectively. If both the pipes are used together, then how long will it take to fill the tank?',
+        'a': '12 Min.',
+        'b': '15 Min.',
+        'c': '25 Min',
+        'd': '50 Min',
+        'correct': 'a'
+    },
+    {
+        'que': 'Two pipes A and B can fill a tank in 15 minutes and 20 minutes respectively. Both the pipes are opened together but after 4 minutes, pipe A is turned off. What is the total time required to fill the tank?',
+        'a': '10 Min, 20 Sec.',
+        'b': '11 Min, 45 Sec.',
+        'c': '12 Min, 30 Sec',
+        'd': '14 Min. 30 Sec.',
+        'correct': 'd'
+    },
+    {
+        'que': 'One pipe can fill a tank three times as fast as another pipe. If together the two pipes can fill the tank in 36 minutes, then the slower pipe alone will be able to fill the tank in:',
+        'a': '81 Min.',
+        'b': '108 min.',
+        'c': '144 Min.',
+        'd': '192 Min.',
+        'correct': 'c'
+    },
+    {
+        'que': 'A tap can fill a tank in 6 hours. After half the tank is filled, three more similar taps are opened. What is the total time taken to fill the tank completely?',
+        'a': '3 Hours 15 Min.',
+        'b': '3 Hours 45 Min.',
+        'c': '4 Hours',
+        'd': '6 hours',
+        'correct': 'b'
+    },
+    {
+        'que': 'Three taps A, B and C can fill a tank in 12, 15 and 20 hours respectively. If A is open all the time and B and C are open for one hour each alternately, the tank will be full in:',
+        'a': '6 hours',
+        'b': '7 hours',
+        'c': '5 hours',
+        'd': '2 hours',
+        'correct': 'b'
+    },
+    {
+        'que': 'Three pipes A, B and C can fill a tank in 6 hours. After working at it together for 2 hours, C is closed and A and B can fill the remaining part in 7 hours. The number of hours taken by C alone to fill the tank is:',
+        'a': '10',
+        'b': '12',
+        'c': '14',
+        'd': '16',
+        'correct': 'c'
+    }
+]
+const Probability = [
+    {
+        'que': 'A problem is given to three students whose chances of solving it are 1/2, 1/3 and 1/4 respectively. what is probililty that the problem will be solved',
+        'a': '1/4',
+        'b': '1/2',
+        'c': '3/4',
+        'd': '7/12',
+        'correct': 'c'
+    },
+    {
+        'que': 'tickets numbered 1 to 20 are mixed up and then a ticket is drawn at random. What is the probility that the ticket drawn has a number which is a multiple of 3 or 5?',
+        'a': '9/20',
+        'b': '1/2',
+        'c': '3/5',
+        'd': '8/15',
+        'correct': 'a'
+    },
+    {
+        'que': 'A bag contains 6 white and 4 blavck balls. 2 balls are drawn at radom. find the probability that they are of same colour.',
+        'a': '1/2',
+        'b': '8/15',
+        'c': '1/9',
+        'd': '7/15',
+        'correct': 'd'
+    },
+    {
+        'que': 'Two cards are drawn at random from a pack of 52 cards. What is the probability that either both are black or both are queen?',
+        'a': '52/221',
+        'b': '55/221',
+        'c': '55/190',
+        'd': '19/221',
+        'correct': 'b'
+    },
+
+    {
+        'que': 'A bag contains 4 white, 5 red and 6 blue balls. Three balls are drawn at random from tha bag. The probability that all of them are red, is:',
+        'a': '2/91',
+        'b': '1/22',
+        'c': '3/22',
+        'd': '2/77',
+        'correct': 'a'
+    },
+    {
+        'que': 'In a lotter, there are 10 prizes ans 25 blank. A lottery is drawn at random. what is the probility of getting a prize?',
+        'a': '2/7',
+        'b': '3/5',
+        'c': '1/2',
+        'd': '5/7',
+        'correct': 'a'
+    },
+    {
+        'que': 'Two dice are tossed. The probability that the total score is a prime number is:',
+        'a': '1/6',
+        'b': '7/9',
+        'c': '5/12',
+        'd': '1/2',
+        'correct': 'c'
+    },
+    {
+        'que': 'From a pack of 52 cards, two cards are drawn together at random. What is the probability of both the cards being kings?',
+        'a': '1/55',
+        'b': '1/221',
+        'c': '25/57',
+        'd': '35/256',
+        'correct': 'b'
+    },
+    {
+        'que': 'In a box, there are 8 red, 7 blue and 6 green balls. One ball is picked up randomly. What is the probability that is neither red nor green',
+        'a': '1/3',
+        'b': '3/5',
+        'c': '8/12',
+        'd': '7/21',
+        'correct': 'a'
+    },
+    {
+        'que': 'One card is drawn at random from a pack of 52 cards. What is the probability that the card drawn is face card(Jack, Queen and King)only?',
+        'a': '1/13',
+        'b': '3/52',
+        'c': '3/13',
+        'd': '9/52',
+        'correct': 'c'
+    }
+]
+let Problems_on_Age = [
+    {
+        'que': 'Father is aged three times more than his son Ronit. After 8 years, he would be two and a half times of Ronits age. After further 8 years how many times would he be of Ronits age?',
+        'a': '2 Times',
+        'b': '3 Times',
+        'c': '4 Times',
+        'd': '5 Times',
+        'correct': 'a'
+    },
+    {
+        'que': 'The sum of ages of 5 children born at the intervals of 3 years each is 50 years. What is the age of the youngest child?',
+        'a': '8 years',
+        'b': '10 years',
+        'c': '4 years',
+        'd': 'None Of The Above',
+        'correct': 'c'
+    },
+    {
+        'que': 'A father said to his son, I was as old as you are at the present at the time of your birth. If the fathers age is 38 years now the sons age five years back was:',
+        'a': '14 years',
+        'b': '19 years',
+        'c': '33 years',
+        'd': '38 years',
+        'correct': 'a'
+    },
+    {
+        'que': 'A man is 24 years older than his son. In two years, his age will be twice the age of his son. The present age of his son is: ',
+        'a': '14 Years',
+        'b': '18 Years',
+        'c': '20 Years',
+        'd': '22 Years',
+        'correct': 'd'
+    },
+    {
+        'que': 'Six years ago, the ratio of the ages of Kunal and Sagar was 6 : 5. Four years hence, the ratio of their ages will be 11 : 10. What is Sagars age at present?',
+        'a': '16 Years',
+        'b': '18 Years',
+        'c': '20 Years',
+        'd': '22 Years',
+        'correct': 'a'
+    },
+    {
+        'que': 'Present ages of Sameer and Anand are in the ratio of 5 : 4 respectively. Three years hence, the ratio of their ages will become 11 : 9 respectively. What is Anands present age in years?',
+        'a': '24 years',
+        'b': '27 years',
+        'c': '40 years',
+        'd': '46 years',
+        'correct': 'a'
+    },
+    {
+        'que': 'The sum of the present ages of a father and his son is 60 years. Six years ago, fathers age was five times the age of the son. After 6 years sons age will be:',
+        'a': '14 years',
+        'b': '13 years',
+        'c': '18 years',
+        'd': '20 years',
+        'correct': 'd'
+    },
+    {
+        'que': 'At present, the ratio between the ages of Arun and Deepak is 4 : 3. After 6 years, Aruns age will be 26 years What is the age of Deepak at present ?',
+        'a': '12 years',
+        'b': '15 years',
+        'c': '19 years',
+        'd': '21 years',
+        'correct': 'b'
+    },
+    {
+        'que': 'Sachin is younger than Rahul by 7 years. If their ages are in the respective ratio of 7 : 9, how old is Sachin?',
+        'a': '16 years',
+        'b': '18 years',
+        'c': '28 years',
+        'd': '24.5 years',
+        'correct': 'd'
+    },
+    {
+        'que': 'A is two years older than B who is twice as old as C. If the total of the ages of A, B and C be 27, then how old is B?',
+        'a': '7 years',
+        'b': '8 years',
+        'c': '9 years',
+        'd': '10 years',
+        'correct': 'd'
+    }
+]
+let Profit_And_Loss = [
+    {
+        'que': 'A shopkeeper takes 10% profit on his goods. He lost 20% of his goods during a theft. What is his loss per cent?',
+        'a': '12%',
+        'b': '15%',
+        'c': '20%',
+        'd': '10%',
+        'correct': 'a'
+    },
+    {
+        'que': 'By selling 100 notebooks, a shopkeeper gains the selling price of 20 notebooks. What is his gain percentage?',
+        'a': '22%',
+        'b': '20%',
+        'c': '25%',
+        'd': 'None Of The Above',
+        'correct': 'c'
+    },
+    {
+        'que': 'Arun sells an object to Benny at a profit of 15%, Benny sells that object to Chandan for ₹1012 and makes a profit of 10%. At what cost did Arun purchase the object?',
+        'a': '₹ 220 ',
+        'b': '₹ 480',
+        'c': '₹ 600',
+        'd': '₹ 800',
+        'correct': 'd'
+    },
+    {
+        'que': 'A television and a washing machine were sold for ₹12500 each. If the television was sold at a gain of 30% and the washing machine at a loss of 30%. Find the overall profit% or loss% on the entire transaction?',
+        'a': '10%',
+        'b': '9%',
+        'c': '8%',
+        'd': '7%',
+        'correct': 'b'
+    },
+    {
+        'que': 'A man sold two steel chairs for ₹ 500. On one, he gains 20% and on the other, he loses 12%. How much does he gain or lose on the whole transaction?',
+        'a': '2%',
+        'b': '25%',
+        'c': '5%',
+        'd': 'None Of The Above',
+        'correct': 'd'
+    },
+    {
+        'que': 'A dishonest shopkeeper pretends to sell his goods at cost price but uses false weights and gains 111/9%. Find the false weight he is using instead of 1kg weight.',
+        'a': '₹ 500',
+        'b': '₹ 200',
+        'c': '₹ 240',
+        'd': 'None Of The Above',
+        'correct': 'd'
+    },
+    {
+        'que': 'Manny bought an AC for ₹ 12160 and paid ₹ 340 for transportation. Then he sold it for ₹12875. Find his profit %.',
+        'a': '1%',
+        'b': '2%',
+        'c': '3%',
+        'd': '4%',
+        'correct': 'c'
+    },
+    {
+        'que': 'If an article is sold for ₹ 178 at a loss of 11%, what should be its selling price in order to earn a profit of 11%?',
+        'a': '₹ 220',
+        'b': '₹ 222.4',
+        'c': '₹ 500',
+        'd': '₹ 600',
+        'correct': 'b'
+    },
+    {
+        'que': 'A cloth merchant sold half of his coth at 20% profit, half of the remaining at 20% loss and the rest was sold at the cost price. Calculate the overall profit% or loss%?',
+        'a': '5%',
+        'b': '2%',
+        'c': '6%',
+        'd': '10%',
+        'correct': 'a'
+    },
+    {
+        'que': 'By selling 90 ball pens for ₹160 a person loses 20%. How many ball pens should be sold for ₹96 so as to have a profit of 20%?',
+        'a': '40%',
+        'b': '60%',
+        'c': '20%',
+        'd': '80%',
+        'correct': 'c'
+    }
+]
+let option;
+let option1;
+let total =10
+let index = 0;
+let right = 0,
+    wrong = 0;
+let attemt=right+wrong;
+let i=0;
+let question_Type = 0;
+const time=document.getElementById("time_taken")
+const score = document.getElementById("score")
+const Que_type = document.getElementById("Que_type_Name")
+const QuesBox = document.getElementById("QuesBox")
+const que_number = document.getElementById("que_number")
+const options = document.querySelectorAll('.options')
+
+// LOAD QUESTION
+setInterval(()=>{
+    if(i<=600){
+        
+        document.getElementById("timer").innerText=i
+        localStorage.setItem('time', i);
+                let time = localStorage.getItem('type');
+    }
+    i++;
+},1000);
+
+const loadquestion = () => {
+    
+  
+    if (index == total) {
+        return endQuiz()
+    }
+    reset()
+// LOAD THE QUESTIONS
+    if(Qtype==1)
+    var data = Pipes_And_cisterns[index]
+    
+    
+    if(Qtype==2)
+    var data = Probability[index]
+    
+    if(Qtype==3)
+    var data = Problems_on_Age[index]
+    
+    if(Qtype==4)
+    var data = Profit_And_Loss[index]
+    option1=data.correct;
+    QuesBox.innerText = `${data.que}`;
+    que_number.innerText = index + 1
+    score.innerText=right
+
+    options[0].nextElementSibling.innerText = data.a;
+    options[1].nextElementSibling.innerText = data.b;
+    options[2].nextElementSibling.innerText = data.c;
+    options[3].nextElementSibling.innerText = data.d;
+}
+
+   // CLICK ON SUBMIT BUTTON
+   
+const submitQuiz = () => {
+    if(Qtype==1)
+    var data = Pipes_And_cisterns[index]
+    
+    if(Qtype==2)
+    var data = Probability[index]
+    
+    if(Qtype==3)
+    var data = Problems_on_Age[index]
+    
+    if(Qtype==4)
+    var data = Profit_And_Loss[index]
+  
+    let ans = getAnswer()
+    if (ans ==data.correct) {
+        right++;   
+    }
+    else {
+        wrong++;
+    }
+
+    index++;
+    set_result();
+    document.getElementById("a").style.backgroundColor='rgba(27, 145, 255,0.3)'; 
+    document.getElementById("b").style.backgroundColor='rgba(27, 145, 255,0.3)'; 
+    document.getElementById("c").style.backgroundColor='rgba(27, 145, 255,0.3)'; 
+    document.getElementById("d").style.backgroundColor='rgba(27, 145, 255,0.3)'; 
+    loadquestion();
+
+    return;
+}
+  //TYPE YOUR NAME
+const Q_Type_Heading=()=>{
+
+    
+if(Qtype==1)
+ Que_type.innerText=('Pipes And Cisterns');
+
+if(Qtype==2)
+Que_type.innerText=('Probability');
+if(Qtype==3)
+Que_type.innerText=('Problems_on_Age');
+if(Qtype==4)
+Que_type.innerText=('rofit_And_Loss');
+}
+
+const getAnswer = () => {
+    let answer;
+    options.forEach(
+        (input) => {
+            if (input.checked) {
+                answer = input.value;
+            }
+        }
+    )
+    return answer;
+    
+}
+//RESER SELECTION OF PREVIOUS QUESTINS
+
+const reset = () => {
+    options.forEach(
+        (input) => {
+            input.checked = false
+        }
+    )
+
+}
+// LOAD RESULT PAGE
+const endQuiz = () => {
+    window.location.href = 'result.html';
+    result();
+
+}
+
+    //FINAL RESULT
+    set_result=()=>{
+
+    localStorage.setItem('Wrong', wrong);
+    localStorage.setItem('Correct', right);
+    }
+clr_on_click1=()=>
+{ 
+        options.forEach(
+            (input) => {
+                if (input.checked) {
+                    option = input.value;
+                }
+            }
+        ) 
+    if (option ==option1) {
+        document.getElementById("a").style.backgroundColor='green';   
+    }
+    else {
+        document.getElementById("a").style.backgroundColor='red';
+    }
+}
+
+clr_on_click2=()=>
+{ 
+        options.forEach(
+            (input) => {
+                if (input.checked) {
+                    option = input.value;
+                }
+            }
+        ) 
+    if (option ==option1) {
+        document.getElementById("b").style.backgroundColor='green';   
+    }
+    else {
+        document.getElementById("b").style.backgroundColor='red';
+    }
+}
+clr_on_click3=()=>
+{ 
+        options.forEach(
+            (input) => {
+                if (input.checked) {
+                    option = input.value;
+                }
+            }
+        ) 
+    if (option ==option1) {
+        document.getElementById("c").style.backgroundColor='green';   
+    }
+    else {
+        document.getElementById("c").style.backgroundColor='red';
+    }
+}
+clr_on_click4=()=>
+{ 
+        options.forEach(
+            (input) => {
+                if (input.checked) {
+                    option = input.value;
+                }
+            }
+        ) 
+    if (option ==option1) {
+        document.getElementById("d").style.backgroundColor='green';   
+    }
+    else {
+        document.getElementById("d").style.backgroundColor='red';
+    }
+}
+
+
+Qtype=localStorage.getItem('type');
+Q_Type_Heading();
+loadquestion();
+;
+
